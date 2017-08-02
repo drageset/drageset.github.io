991ffe3bd8680b40d4cfa3ac806b38671de12b1b
drageset <olve.drageset@gmail.com>
2017-08-03 01:52:17 +0200

    total menu revamp

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 //Make the NAV bar functional
 
 var hamburger = document.getElementById('nav-icon3');
-hamburger.addEventListener('click', function(e) {
+var hamwrapper = document.getElementById('ham-wrapper');
+hamwrapper.addEventListener('click', function(e) {
 	hamburger.classList.toggle("open");
 	var hamMenu = document.getElementById('hamburger-nav');
 	hamMenu.classList.toggle("hidden");
